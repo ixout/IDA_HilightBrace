b5c90800f13df078489e62264c649dd0708745cb
ixout <47586428+ixout@users.noreply.github.com>
2025-12-14 15:41:42 +0800

    change

--- src/MatchingBrace.hpp
+++ src/MatchingBrace.hpp
@@ -65,10 +65,15 @@ public:
     }
     return result;
   }
-  static unsigned int GetColor() {
-    static int colors_[10] = { 0xCD853F, 0xFFD700, 0xFFFF00, 0x7FFF00, 0xA5BF8E, 0x00BFFF, 0x8A2BE2, 0xFFEFD5, 0xFFC0CB, 0xD8BFD8 };
-    srand(time(NULL));
-    return colors_[rand() % 10];
+    static unsigned int GetColor() {
+    static std::mt19937 rng{ std::random_device{}() };
+    static std::uniform_int_distribution<int> dist(128, 255);
+
+    uint32_t r = dist(rng);
+    uint32_t g = dist(rng);
+    uint32_t b = dist(rng);
+
+    return (r << 16) | (g << 8) | b;
   }
 private:
   MatchingBrace() = default;
